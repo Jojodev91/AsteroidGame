84b05f55e8087963dec760b24d6ec098e8e653db
Jojodev91 <joeldel91@gmail.com>
2025-05-22 22:27:19 -0400

    added player triangle to the screen, player.py, circleshape.py

--- main.py
+++ main.py
@@ -3,18 +3,29 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
+
 
 def main():
     print('"Starting Asteroids!"')
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.time.Clock()
+    dt = 0
+
+    player_ship = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player_ship.draw(screen)
         pygame.display.flip()
+    dt = (pygame.tick(60)/1000)
+
 
 if __name__ == "__main__":
         main()

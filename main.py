66bb8b3e3e94cdbcaab7f356c4ddb1558fd8d865
Jojodev91 <joeldel91@gmail.com>
2025-05-27 22:01:53 -0400

    Implimented collision and asteroids

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+import sys
 import pygame
 from constants import *
 from player import *
-
+from asteroid import *
+from asteroidfield import *
 
 def main():
     print('"Starting Asteroids!"')
@@ -15,45 +17,35 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
-    
     # Groups
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
     Player.containers = (updatable, drawable)
+    AsteroidField.containers = (updatable,)
 
     player_ship = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
-
+    asteroid_field = AsteroidField()
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
+        
         updatable.update(dt)
-        for draw in drawable:
-             draw.draw(screen)
+        for asteroid in asteroids:
+            if asteroid.collision(player_ship):
+                print("Game Over!")
+                sys.exit()
+        screen.fill("black")
+
+        for obj in drawable:
+             obj.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60)/1000
-
 
 if __name__ == "__main__":
         main()
-
-
-'''
-# Groups
-    updatable = pygame.sprite.Group()
-    drawable = pygame.sprite.Group()
-    player_ship.containers = (updatable, drawable)
-
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
-        screen.fill("black")
-        updatable.update(dt)
-        for draw in drawable:
-             draw.draw(screen)
-        pygame.display.flip()
-        dt = clock.tick(60)/1000
-'''
